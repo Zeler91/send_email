269288ddf27713af09ea6e5d3ad033dc395115a4
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2020-02-02 15:59:32 +0300

    Update settings.py

--- send_email/settings.py
+++ send_email/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's59-sdx!r*#vjq9u(7njr%y$%aoo$g5ny90nheh95_&o&ry6fh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
